54f7acc3ce2ebfaeaa86712ad997d413321c1754
Sophie <70263883+sophie-03@users.noreply.github.com>
2025-10-10 17:26:52 +0100

    Update slurm_submission.sh

--- Genome_wide_associations/CNV_gwas_validate/slurm_submission.sh
+++ Genome_wide_associations/CNV_gwas_validate/slurm_submission.sh
@@ -2,10 +2,10 @@
 #SBATCH --export=ALL # export all environment variables to the batch job
 #SBATCH --mail-type=END # send email at job completion
 #SBATCH --mail-user=s.matthews5@universityofgalway.ie # email address
-#SBATCH --job-name=C50_dels #job name
+#SBATCH --job-name=C61_dels #job name
 #SBATCH -D /data4/smatthews/pheWAS/cnv_GWAS # set working directory to
 
 module load Anaconda3/2024.02-1
 conda activate cnvGWAS
 
-Rscript /data4/smatthews/pheWAS/github_gene_centric_cnv_analysis/Genome_wide_associations/CNV_gwas_validate/CNV_GWAS_dels.R C50
+Rscript /data4/smatthews/pheWAS/github_gene_centric_cnv_analysis/Genome_wide_associations/CNV_gwas_validate/CNV_GWAS_dels.R C61
