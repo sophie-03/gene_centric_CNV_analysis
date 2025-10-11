e3e50db1bde2f20cd81f02b0d2b5c9f90472cabb
Sophie <70263883+sophie-03@users.noreply.github.com>
2025-10-11 15:11:17 +0100

    Update slurm_submission.sh

--- Genome_wide_associations/CNV_gwas_validate/slurm_submission.sh
+++ Genome_wide_associations/CNV_gwas_validate/slurm_submission.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-#SBATCH --export=ALL # export all environment variables to the batch job
-#SBATCH --mail-type=END # send email at job completion
-#SBATCH --mail-user=s.matthews5@universityofgalway.ie # email address
-#SBATCH --job-name=C44_dups #job name
-#SBATCH -D /data4/smatthews/pheWAS/cnv_GWAS # set working directory to
+#SBATCH --export=ALL
+#SBATCH --mail-type=END
+#SBATCH --mail-user=s.matthews5@universityofgalway.ie
+#SBATCH --job-name=cancer_dels
+#SBATCH -D /data4/smatthews/pheWAS/cnv_GWAS
+#SBATCH --array=1-40         # one per CNV part file
+#SBATCH -o logs/cancer_dels_%A_%a.out
 
 module load Anaconda3/2024.02-1
 conda activate cnvGWAS
 
-Rscript /data4/smatthews/pheWAS/github_gene_centric_cnv_analysis/Genome_wide_associations/CNV_gwas_validate/CNV_GWAS_dups.R C44
+PHENO="cancer"
+PART=${SLURM_ARRAY_TASK_ID}
+
+Rscript /data4/smatthews/pheWAS/github_gene_centric_cnv_analysis/Genome_wide_associations/CNV_gwas_validate/CNV_GWAS_dels.R $PHENO $PART
