417ec03a062b6d442062d35d40151b209d40ba92
Sophie <70263883+sophie-03@users.noreply.github.com>
2025-10-11 15:10:00 +0100

    Update CNV_GWAS_dels.R

--- Genome_wide_associations/CNV_gwas_validate/CNV_GWAS_dels.R
+++ Genome_wide_associations/CNV_gwas_validate/CNV_GWAS_dels.R
@@ -8,37 +8,11 @@ args <- commandArgs(trailingOnly = TRUE)
 
 # Phenotype name
 pheno_name <- args[1]
+part_num <- as.integer(args[2])
 
 #read in cnv calls
-cnvs <- read.table("/data4/smatthews/pheWAS/cnv_GWAS/cnv_calls.bed")
-colnames(cnvs) <- c("chr","start","end","state","sample_id","UKB_id")
-
-#group the calls by sample ID and convert them to a GRangesList
-grl <- GenomicRanges::makeGRangesListFromDataFrame(cnvs, 
-                                            split.field="sample_id", keep.extra.columns=TRUE)
-#sort
-grl <- GenomicRanges::sort(grl)
-
-#summarize cnv calls to cnv regions
-cnvrs <- populationRanges(grl, density=0.1)
-
-#convert regions to df
-cnvrs_df <- as.data.frame(cnvrs)
-cnvrs_df$region_id <- paste0("region_",1:nrow(cnvrs_df))
-
-# Convert both to data.tables
-cnvs_dt <- as.data.table(cnvs)
-cnvrs_dt <- as.data.table(cnvrs_df)
-
-
-# combine cnv calls with cnv regions
-cnvs_with_regions <- cnvs_dt[cnvrs_dt, 
-                             on = .(chr == seqnames, 
-                                    start >= start, 
-                                    end <= end),
-                             nomatch = NULL,  # Remove non-matches
-                             .(chr = seqnames, start = i.start, end = i.end, 
-                               state, sample_id, UKB_id, region_id, freq, type)]
+cnv_file <- paste0("/data4/smatthews/pheWAS/cnv_GWAS/cnv_regions_splits/cnv_regions_part", part_num, ".txt")
+cnvs_with_regions <- fread(cnv_file)
 
 # get data for GWAS
 pheno_file <- paste0("/data4/smatthews/pheWAS/pheno_", pheno_name, "/", pheno_name, "_cases.txt")
@@ -107,5 +81,26 @@ for(region_no in all_regions) {
   })
 }
 
+
+## WRITE OUTPUT
+# ---- Safe append to master output ----
 output_file <- paste0("/data4/smatthews/pheWAS/cnv_GWAS/", pheno_name, "_del_logistic_results.txt")
-write.table(results_df, output_file, col.names = TRUE, row.names = FALSE, quote = FALSE)
+lock_file <- paste0(output_file, ".lock")
+
+# Write results to a temporary file first
+tmp_file <- tempfile(pattern = paste0("part", part_num, "_"), fileext = ".txt")
+fwrite(results_df, tmp_file, sep = "\t", col.names = (part_num == 1))  # header only for first
+
+# Append safely using a lock
+lock <- lock(file = lock_file, exclusive = TRUE, timeout = 6000)
+cat("Appending results from part", part_num, "to master file...\n")
+
+if (!file.exists(output_file)) {
+  # if master file doesn’t exist, include header
+  fwrite(results_df, output_file, sep = "\t", col.names = TRUE, append = FALSE)
+} else {
+  # append without header
+  fwrite(results_df, output_file, sep = "\t", col.names = FALSE, append = TRUE)
+}
+
+unlock(lock)
