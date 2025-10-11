c6e06c2b2573b691792b2f4d08824e96e76648f2
Sophie <70263883+sophie-03@users.noreply.github.com>
2025-10-11 17:58:52 +0100

    Update slurm_submission.sh

--- Genome_wide_associations/CNV_gwas_validate/slurm_submission.sh
+++ Genome_wide_associations/CNV_gwas_validate/slurm_submission.sh
@@ -2,16 +2,16 @@
 #SBATCH --export=ALL
 #SBATCH --mail-type=END
 #SBATCH --mail-user=s.matthews5@universityofgalway.ie
-#SBATCH --job-name=C44_dels
+#SBATCH --job-name=C50_dels
 #SBATCH -D /data4/smatthews/pheWAS/cnv_GWAS
 #SBATCH --array=1-40         # one per CNV part file
-#SBATCH -o logs/c44_dels_%A_%a.out
+#SBATCH -o logs/C50_dels_%A_%a.out
 #SBATCH --nice=100
 
 module load Anaconda3/2024.02-1
 conda activate cnvGWAS
 
-PHENO="C44"
+PHENO="C50"
 PART=${SLURM_ARRAY_TASK_ID}
 
 Rscript /data4/smatthews/pheWAS/github_gene_centric_cnv_analysis/Genome_wide_associations/CNV_gwas_validate/CNV_GWAS_dels.R $PHENO $PART
