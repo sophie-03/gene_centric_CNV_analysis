0fdca23b193618703d91cc4e9979624cf73bb187
Sophie <70263883+sophie-03@users.noreply.github.com>
2025-10-16 21:23:34 +0100

    Update slurm_submission.sh

--- Genome_wide_associations/CNV_gwas_validate/slurm_submission.sh
+++ Genome_wide_associations/CNV_gwas_validate/slurm_submission.sh
@@ -2,10 +2,10 @@
 #SBATCH --export=ALL
 #SBATCH --mail-type=END
 #SBATCH --mail-user=s.matthews5@universityofgalway.ie
-#SBATCH --job-name=dvt_dels
+#SBATCH --job-name=pulm_emb_dels
 #SBATCH -D /data4/smatthews/pheWAS/cnv_GWAS
 #SBATCH --array=1-40         # one per CNV part file
-#SBATCH -o logs/dvt_dels_%A_%a.out
+#SBATCH -o logs/pulm_emb_dels_%A_%a.out
 #SBATCH --nice=100
 #SBATCH --partition=highmem,normal
 #SBATCH --exclude=cn070,cn071,cn072,cn073,cn074,cn075,cn076,cn100
@@ -14,7 +14,7 @@
 module load Anaconda3/2024.02-1
 conda activate cnvGWAS
 
-PHENO="dvt"
+PHENO="pulm_emb"
 PART=${SLURM_ARRAY_TASK_ID}
 
 Rscript /data4/smatthews/pheWAS/github_gene_centric_cnv_analysis/Genome_wide_associations/CNV_gwas_validate/CNV_GWAS_dels.R $PHENO $PART
