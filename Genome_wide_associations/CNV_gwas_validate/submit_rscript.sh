744f259d80bbe86204c41e5e2af847621cdfa632
Sophie <70263883+sophie-03@users.noreply.github.com>
2025-11-11 16:40:07 +0000

    Add SLURM script for CNV GWAS validation

---
+++ Genome_wide_associations/CNV_gwas_validate/submit_rscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --export=ALL
+#SBATCH --mail-type=END
+#SBATCH --mail-user=s.matthews5@universityofgalway.ie
+#SBATCH --job-name=cnvranges
+#SBATCH -D /data4/smatthews/pheWAS/cnv_GWAS
+#SBATCH -o logs/cnvrangesRO.out
+#SBATCH --cpus-per-task=16
+
+module load Anaconda3/2024.02-1
+conda activate cnvGWAS
+
+Rscript /data4/smatthews/pheWAS/github_gene_centric_cnv_analysis/Genome_wide_associations/CNV_gwas_validate/CNVRanges_RO_dels.R
