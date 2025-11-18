168234f7f651cdf1e49f97d28cbd9e2f3f001ea1
Sophie <70263883+sophie-03@users.noreply.github.com>
2025-11-18 13:57:34 +0000

    Update submit_rscript.sh

--- Genome_wide_associations/CNV_gwas_validate/submit_rscript.sh
+++ Genome_wide_associations/CNV_gwas_validate/submit_rscript.sh
@@ -2,12 +2,12 @@
 #SBATCH --export=ALL
 #SBATCH --mail-type=END
 #SBATCH --mail-user=s.matthews5@universityofgalway.ie
-#SBATCH --job-name=RO_0.7_regions
+#SBATCH --job-name=D_0.1_regions
 #SBATCH -D /data4/smatthews/pheWAS/cnv_GWAS
-#SBATCH -o logs/cnvrangesRO.out
+#SBATCH -o logs/cnvrangesD_0.1.out
 #SBATCH --cpus-per-task=16
 
 module load Anaconda3/2024.02-1
 conda activate cnvGWAS
 
-Rscript /data4/smatthews/pheWAS/github_gene_centric_cnv_analysis/Genome_wide_associations/CNV_gwas_validate/CNVRanges_RO_dels.R
+Rscript /data4/smatthews/pheWAS/github_gene_centric_cnv_analysis/Genome_wide_associations/CNV_gwas_validate/CNVRanges_dels.R
