7aa953d4e5bdff4b1d4d57aaa8779993449e0e4a
Sophie <70263883+sophie-03@users.noreply.github.com>
2026-01-07 10:46:32 +0000

    Add bedtools.sh script for gene deletion analysis

---
+++ CDS/bedtools.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --export=ALL
+#SBATCH --mail-type=END
+#SBATCH --mail-user=s.matthews5@universityofgalway.ie
+#SBATCH --job-name=bedtools
+#SBATCH -D /data4/smatthews/pheWAS/CDS
+
+module load Anaconda3/2024.02-1
+conda activate cnvGWAS
+
+cd /data4/smatthews/pheWAS/CDS/
+
+# get genes where the longest cds is overlapped by a deletion
+bedtools intersect -wa -wb -a longest_cds.bed -b cnv_calls.bed -filenames > intersect_cds_cnvs.txt
