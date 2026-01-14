861cf9ae66fecf1c9c451d8049cd4ec0100172fc
Sophie <70263883+sophie-03@users.noreply.github.com>
2026-01-14 15:40:27 +0000

    Update bedtools.sh

--- CDS/bedtools.sh
+++ CDS/bedtools.sh
@@ -11,4 +11,4 @@ conda activate cnvGWAS
 cd /data4/smatthews/pheWAS/CDS/
 
 # get genes where the longest cds is overlapped by a deletion
-bedtools intersect -wa -wb -a longest_cds.bed -b cnv_calls.bed -filenames > intersect_cds_cnvs.txt
+bedtools intersect -wa -wb -a canonical_transcripts_CDS.bed -b cnv_calls.bed -filenames > intersect_cds_cnvs.txt
